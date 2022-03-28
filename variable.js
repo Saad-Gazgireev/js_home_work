605512f3474e3775f3909cfb6752618d737ddce2
Saad Gazgireev <gazgireev92@gmail.com>
2022-03-28 14:18:41 +0300

    задача 1.1

---
+++ variable.js
@@ -0,0 +1,29 @@
+// let название игры superMario
+let superMario
+
+// let бегать и собирать монеты и грибы, перепрыгивая через ямы и гумбы
+let gameName
+
+// let версия игры
+let version_1
+
+// let имя персонажа
+let characterName 
+
+// let имя разработчика игры
+let developerName
+
+// let язык програм-я на котором написана игра
+let languageName
+
+// let версия языка на котором написана игра
+let languageVersion
+
+// let название карты на которой происходит действия игры
+let mapName
+
+// let сайт разработчика игры
+let developerSite
+
+// let имя языка интерфейса
+let interfaceLanguage
