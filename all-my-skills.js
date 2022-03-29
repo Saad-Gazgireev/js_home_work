62f89ae10f59f929acbef4542a559909af0a957c
Saad Gazgireev <gazgireev92@gmail.com>
2022-03-29 16:18:19 +0300

    Задача 3.2

---
+++ all-my-skills.js
@@ -0,0 +1,10 @@
+let myAge = 30 
+const year = 2022
+let name = 'Саад'
+let lastName = "Gazgireev"
+let book = "Евгений Онегин"
+let output = 'Привет, меня зовут ' + name + ", мне " + myAge + ' лет.'
+console.log(output)
+console.log(`Привет, меня зовут  ${name} и я ${year - myAge} года рождения`)
+
+
