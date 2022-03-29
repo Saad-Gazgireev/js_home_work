bc81adf9153d6598f9e1fe05cbbbccc188ffc4f7
Saad Gazgireev <gazgireev92@gmail.com>
2022-03-29 16:37:01 +0300

    Задача 3.3

--- all-my-skills.js
+++ all-my-skills.js
@@ -1,10 +1,11 @@
+// Пример конкатенации строк 
 let myAge = 30 
 const year = 2022
 let name = 'Саад'
-let lastName = "Gazgireev"
+let lastName = "Газгиреев"
 let book = "Евгений Онегин"
 let output = 'Привет, меня зовут ' + name + ", мне " + myAge + ' лет.'
 console.log(output)
-console.log(`Привет, меня зовут  ${name} и я ${year - myAge} года рождения`)
+console.log(`Привет, меня зовут  ${name} ${lastName} и я ${year - myAge} года рождения`)
 
 
