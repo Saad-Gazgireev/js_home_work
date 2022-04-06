cdaadaee60afce8a83dac700016f2f54d532382f
Saad Gazgireev <gazgireev92@gmail.com>
2022-04-06 11:46:48 +0300

    Arrays Задача 1.1

---
+++ creat-array.js
@@ -0,0 +1,15 @@
+let empty = []
+let num = [10, 20, 30]
+let strings = ['Saad', 'Said', 'Saipi']
+let strNums = ['dada', 'sasa', 'rara', 1, 2, 3]
+let groupMate = ['Ker', 'aza', 'kaka', 'tuhan']
+let allDataTypes = [1, 'mama', null, undefined, true, false]
+let arrayInArray = [
+    [],
+    [],
+    [],
+]
+let arraysNum = [
+    [1,2,3],
+    [4,5,6],
+]
